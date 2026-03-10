34e4b257a2e53c5799bae8427cc6b318f83b6577
WillSantosNe <williamsantosstella@gmail.com>
2026-03-10 20:20:17 -0300

    chore: initial project structure

---
+++ src/test/java/com/finanzen/api/FinanzenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzen.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
