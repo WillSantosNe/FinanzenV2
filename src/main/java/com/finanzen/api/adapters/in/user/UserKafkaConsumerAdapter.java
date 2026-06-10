59d0e003de200654cfe626795b4c9f29553ed6a5
WillSantosNe <williamsantosstella@gmail.com>
2026-06-10 20:35:33 -0300

    feat(messaging): implement Kafka consumer adapter for user registration
    Adds UserKafkaConsumerAdapter to listen to the 'user-created' topic in real-time and simulate asynchronous welcome notifications.

--- src/main/java/com/finanzen/api/adapters/in/user/UserKafkaConsumerAdapter.java
+++ src/main/java/com/finanzen/api/adapters/in/user/UserKafkaConsumerAdapter.java
@@ -1,4 +1,35 @@
 package com.finanzen.api.adapters.in.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.finanzen.api.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserKafkaConsumerAdapter {
+
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "user-created", groupId = "finanzen-group")
+    public void consumeUserCreated(String payload){
+        try{
+            User user = objectMapper.readValue(payload, User.class);
+
+            // Simulando processamento de disparo de email
+            System.out.println("[EMAIL SERVICE] -> Preparing financial alert notification...");
+
+            System.out.println("Sending email to: " +  user.getEmail());
+
+            System.out.println("Message Content: Hi! Your user for was successfully processed!");
+
+            System.out.println("[EMAIL SERVICE] -> Notification delivered successfully for User ID: "
+                    +  user.getId());
+        } catch (Exception e) {
+            // Garante que um erro de conversão não derrube o container de escuta do Kafka
+            System.out.println("Error while deserializing or processing user event from Kafka - " + e);
+        }
+    }
+
 }
