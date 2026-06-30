7a39fa1d744c466c69d7f3661fbe43ef488b031b
WillSantosNe <williamsantosstella@gmail.com>
2026-06-30 17:17:59 -0300

    fix(adapters): add @Repository annotation in JpaAccountRepository

--- src/main/java/com/finanzen/api/adapters/out/account/JpaAccountRepository.java
+++ src/main/java/com/finanzen/api/adapters/out/account/JpaAccountRepository.java
@@ -3,9 +3,11 @@ package com.finanzen.api.adapters.out.account;
 import com.finanzen.api.application.dto.common.PageResult;
 import com.finanzen.api.domain.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface JpaAccountRepository extends JpaRepository<JpaAccountEntity, Long> {
 
     Optional<JpaAccountEntity> findByAccountNumber(String accountNumber);
