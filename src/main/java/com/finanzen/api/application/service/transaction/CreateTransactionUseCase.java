815825cba2da3f44e5186e4171cd08e92506008d
WillSantosNe <williamsantosstella@gmail.com>
2026-07-12 14:35:08 -0300

    feat(transaction): link transaction creation to account balance validation

--- src/main/java/com/finanzen/api/application/service/transaction/CreateTransactionUseCase.java
+++ src/main/java/com/finanzen/api/application/service/transaction/CreateTransactionUseCase.java
@@ -1,13 +1,19 @@
 package com.finanzen.api.application.service.transaction;
 
+import com.finanzen.api.application.ports.in.account.FindAccountByIdPort;
+import com.finanzen.api.application.ports.in.account.UpdateAccountBalancePort;
 import com.finanzen.api.application.ports.in.transaction.CreateTransactionPort;
+import com.finanzen.api.application.ports.in.transaction.FindTransactionByIdPort;
 import com.finanzen.api.application.ports.out.transaction.TransactionEventPublisherPort;
 import com.finanzen.api.application.ports.out.transaction.TransactionRepositoryPort;
+import com.finanzen.api.domain.account.Account;
 import com.finanzen.api.domain.transaction.Transaction;
+import com.finanzen.api.domain.transaction.TransactionType;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 /**
@@ -25,6 +31,8 @@ public class CreateTransactionUseCase implements CreateTransactionPort {
 
     private final TransactionRepositoryPort repository;
     private final TransactionEventPublisherPort eventPublisher;
+    private final FindAccountByIdPort findAccountByIdPort;
+    private final UpdateAccountBalancePort  updateAccountBalancePort;
 
     /**
      * Executes the use case to create a new transaction.
@@ -38,6 +46,19 @@ public class CreateTransactionUseCase implements CreateTransactionPort {
     public Transaction create(Transaction transaction, String userEmail) {
         transaction.setCreatedAt(LocalDateTime.now());
         transaction.setUserEmail(userEmail);
+
+        // Verifica se conta existe
+        findAccountByIdPort.findById(transaction.getAccountId(), userEmail);
+
+        // Calculo do delta
+        BigDecimal balanceDelta = transaction.getType() == TransactionType.EXPENSE
+                ? transaction.getAmount().negate()
+                : transaction.getAmount();
+
+        // Muda saldo da conta
+        Account account = updateAccountBalancePort.execute(transaction.getAccountId(), balanceDelta, userEmail);
+
+        // Salva transaction
         Transaction savedTransaction = repository.save(transaction);
 
         // Disparando evento assincrono para o Kafka com o Transaction ID
