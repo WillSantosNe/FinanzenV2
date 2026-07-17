7980e4b48314af7eab5235a21cf4e6445857f582
WillSantosNe <williamsantosstella@gmail.com>
2026-07-17 17:12:13 -0300

    feat(account): consume transaction events to process balance updates

--- src/main/java/com/finanzen/api/adapters/in/account/AccountKafkaConsumerAdapter.java
+++ src/main/java/com/finanzen/api/adapters/in/account/AccountKafkaConsumerAdapter.java
@@ -1,4 +1,39 @@
 package com.finanzen.api.adapters.in.account;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.finanzen.api.application.ports.in.account.UpdateAccountBalancePort;
+import com.finanzen.api.domain.account.Account;
+import com.finanzen.api.domain.transaction.Transaction;
+import com.finanzen.api.domain.transaction.TransactionType;
+import lombok.AllArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@AllArgsConstructor
 public class AccountKafkaConsumerAdapter {
+
+    private final ObjectMapper objectMapper;
+    private final UpdateAccountBalancePort updateAccountBalancePort;
+
+    @KafkaListener(topics = "transaction-events", groupId = "finanzen-group")
+    public void consumeTransactionCreated(String payload)
+    {
+        try {
+            Transaction transaction = objectMapper.readValue(payload, Transaction.class);
+
+            BigDecimal balanceDelta = transaction.getType() == TransactionType.EXPENSE
+                ? transaction.getAmount().negate()
+                : transaction.getAmount();
+
+            Account account = updateAccountBalancePort.execute(transaction.getAccountId(), balanceDelta, transaction.getUserEmail());
+            System.out.println("[UPDATE ACCOUNT BALANCE SERVICE] -> Account: " + account.getAccountNumber()
+                    + " | Balance: " + account.getBalance());
+        }
+        catch (Exception e) {
+            System.out.println("Error while deserializing or processing transaction event from Kafka - " + e);
+        }
+    }
 }
