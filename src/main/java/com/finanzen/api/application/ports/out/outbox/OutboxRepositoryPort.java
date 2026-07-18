e39698878646bb7b17490f66b45ffbaddc496931
WillSantosNe <williamsantosstella@gmail.com>
2026-07-18 15:59:19 -0300

    feat(outbox): enable spring scheduling and define unprocessed events port

--- src/main/java/com/finanzen/api/application/ports/out/outbox/OutboxRepositoryPort.java
+++ src/main/java/com/finanzen/api/application/ports/out/outbox/OutboxRepositoryPort.java
@@ -1,7 +1,11 @@
-package com.finanzen.api.application.ports.out;
+package com.finanzen.api.application.ports.out.outbox;
 
+import com.finanzen.api.adapters.out.outbox.JpaOutboxEntity;
 import com.finanzen.api.domain.outbox.OutboxEvent;
+
+import java.util.List;
 
 public interface OutboxRepositoryPort {
     OutboxEvent save(OutboxEvent event);
+    List<OutboxEvent> findUnprocessedEvents();
 }
