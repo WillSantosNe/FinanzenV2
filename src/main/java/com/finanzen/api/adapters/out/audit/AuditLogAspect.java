4dcc9fb905ba32d77b877892d8a01c2a46298444
WillSantosNe <williamsantosstella@gmail.com>
2026-07-25 23:40:54 -0300

    feat(audit): implement security audit log mechanism via spring aop

--- src/main/java/com/finanzen/api/adapters/out/audit/AuditLogAspect.java
+++ src/main/java/com/finanzen/api/adapters/out/audit/AuditLogAspect.java
@@ -1,4 +1,40 @@
 package com.finanzen.api.adapters.out.audit;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Aspect
+@Component
+@AllArgsConstructor
 public class AuditLogAspect {
+
+    private final MongoAuditRepository mongoAuditRepository;
+
+    // Pointcut - intercepta qualquer execução sinalizada
+    @AfterReturning(
+            pointcut = "execution(* com.finanzen.api.application.service.account.UpdateAccountBalanceUseCase.execute(..))",
+            returning = "result"
+    )
+    public void logTransaction(JoinPoint joinPoint, Object result) {
+
+        // Joinpoint - capturando parâmetros enviados para o execute()
+        Object[] args = joinPoint.getArgs();
+
+        // Extraindo parametros
+        Long accountId = (Long) args[0];
+        BigDecimal amount = (BigDecimal) args[1];
+        String userEmail = (String) args[2];
+
+        AuditLogEntity auditLogEntity = new AuditLogEntity(null, accountId, amount, userEmail, LocalDateTime.now());
+
+        mongoAuditRepository.save(auditLogEntity);
+
+        System.out.println("[AUDIT LOG] -> Rastro de auditoria salvo no MongoDB silenciosamente!");
+    }
 }
